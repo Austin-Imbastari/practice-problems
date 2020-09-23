b9b7bde3823a793e2a34f0b147d0d57083c0cd5d
Austin Imbastari <austinimbastari@gmail.com>
2020-09-23 10:26:00 -0700

    completed digit

--- main.js
+++ main.js
@@ -4,13 +4,11 @@
 // 2. the function recieves an array [] of a number
 // 3. the array will be used to filter the numbere value in the arguement
 // 4. return an array
-// 5. i know ill need sometype of storage.
+// 5. i know ill need sometype of storage. empty array
 // 6. will neeed a loop. to looop through the array.
-
-
-
-
-
+// 7. if else statement to determine what to push.
+// 8. i know i will have to push the values into a new array .push?
+// 9. if the numbersarray === number filter push it into results.
 
   function digitFilter(numbersArray, numberFilter) {
     var result = []
@@ -23,7 +21,6 @@
 
     return result
   }
-
 
   console.log(digitFilter([23, 312, 24, 243, 43], 3));
   console.log(digitFilter([23, 315, 231, 54, 72], 2));
